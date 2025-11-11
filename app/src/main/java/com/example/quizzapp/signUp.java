16721ff7793713b5f226da91086b13e7400ae934
Thai Nguyen <thai2862005@gmail.com>
2025-11-11 11:23:16 +0700

    custom input sign up

--- app/src/main/java/com/example/quizzapp/signUp.java
+++ app/src/main/java/com/example/quizzapp/signUp.java
@@ -2,6 +2,9 @@ package com.example.quizzapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,12 +14,66 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 public class signUp extends AppCompatActivity {
     TextView txtSignIn;
+    EditText etPassword;
+    EditText etConfirmPassword;
+    Boolean isPasswordVisible = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
         txtSignIn = findViewById(R.id.txtSignin);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+
+        etPassword.setOnTouchListener((v, event) -> {
+            final int DRAWABLE_RIGHT = 2; // icon nằm bên phải
+
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (etPassword.getRight() - etPassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                    if (isPasswordVisible) {
+                        // Ẩn mật khẩu
+                        etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        etPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.outline_accessible_off, 0);
+                        isPasswordVisible = false;
+                    } else {
+                        // Hiện mật khẩu
+                        etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                        etPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.outline_accessibility_on, 0);
+                        isPasswordVisible = true;
+                    }
+
+                    etPassword.setSelection(etPassword.getText().length());
+                    return true;
+                }
+            }
+            return false;
+        });
+        etConfirmPassword.setOnTouchListener((v, event) -> {
+            final int DRAWABLE_RIGHT = 2; // icon nằm bên phải
+
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (etConfirmPassword.getRight() - etConfirmPassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                    if (isPasswordVisible) {
+                        // Ẩn mật khẩu
+                        etConfirmPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        etConfirmPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.outline_accessible_off, 0);
+                        isPasswordVisible = false;
+                    } else {
+                        // Hiện mật khẩu
+                        etConfirmPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                        etConfirmPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.outline_accessibility_on, 0);
+                        isPasswordVisible = true;
+                    }
+
+                    etConfirmPassword.setSelection(etConfirmPassword.getText().length());
+                    return true;
+                }
+            }
+            return false;
+        });
         txtSignIn.setOnClickListener(v -> {
             Intent intent = new Intent(signUp.this, login.class);
             startActivity(intent);
