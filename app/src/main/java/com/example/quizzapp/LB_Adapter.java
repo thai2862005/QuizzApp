438dc47c3ed4934f0dc7887f601bf63291706a52
Thai Nguyen <thai2862005@gmail.com>
2025-11-15 11:14:43 +0700

    add event change list view by userId

--- app/src/main/java/com/example/quizzapp/LB_Adapter.java
+++ app/src/main/java/com/example/quizzapp/LB_Adapter.java
@@ -3,10 +3,13 @@ package com.example.quizzapp;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -36,7 +39,8 @@ public class LB_Adapter extends ArrayAdapter<LB_items> {
         TextView tvRank = row.findViewById(R.id.tv_rank);
         TextView tvName = row.findViewById(R.id.tv_name);
         TextView tvPoints = row.findViewById(R.id.tv_points);
-
+       LinearLayout linearOp = row.findViewById(R.id.linearOption);
+       FrameLayout frameOp = row.findViewById(R.id.frameOption);
         LB_items lb = lb_items.get(position);
 
         tvRank.setText(lb.getRank());
@@ -44,14 +48,26 @@ public class LB_Adapter extends ArrayAdapter<LB_items> {
         tvPoints.setText(lb.getPoints());
 
         if (lb.getUserId() == currentUserId) {
-            row.setBackgroundColor(Color.parseColor("#FFFACD"));
-            tvName.setTextColor(Color.RED);
-            tvPoints.setTextColor(Color.RED);
-        } else {
-            row.setBackgroundColor(Color.WHITE);
+            linearOp.setBackgroundResource(R.drawable.bg_op_true);
+            frameOp.setBackgroundResource(R.drawable.bg_op4);
+            tvName.setText("You");
+            tvRank.setTypeface(tvName.getTypeface(), Typeface.BOLD);
+            tvName.setTypeface(tvName.getTypeface(), Typeface.BOLD);
+            tvPoints.setTypeface(tvPoints.getTypeface(), Typeface.BOLD);
+
             tvName.setTextColor(Color.BLACK);
-            tvPoints.setTextColor(Color.BLACK);
+            tvPoints.setTextColor(Color.GRAY);
+            tvRank.setTextColor(Color.WHITE);
+        } else {
+            linearOp.setBackgroundResource(R.drawable.bg_op1);
+
+            tvName.setTypeface(Typeface.DEFAULT);
+            tvPoints.setTypeface(Typeface.DEFAULT);
+
+            tvName.setTextColor(Color.BLACK);
+            tvPoints.setTextColor(Color.GRAY);
         }
+
 
         return row;
     }
