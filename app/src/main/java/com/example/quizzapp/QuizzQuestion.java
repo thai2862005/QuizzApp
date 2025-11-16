08ce6a197576f5587d24509fffbc05a6eb9fb134
Tuyet Nhi <vutuyetnhi0911@gmail.com>
2025-11-16 18:44:50 +0700

    fix quizzLayout && quizzQuestion.java

--- app/src/main/java/com/example/quizzapp/QuizzQuestion.java
+++ app/src/main/java/com/example/quizzapp/QuizzQuestion.java
@@ -1,8 +1,11 @@
 package com.example.quizzapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -13,16 +16,26 @@ public class QuizzQuestion extends AppCompatActivity {
     ListView lvQuizz;
     ArrayList<Quizz_items> arrQuizzs;
     Quizz_Adapter adapter;
-
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        EdgeToEdge.enable(this);
 //        if (!AuthToken.checkAuth(this)) {
 //            finish(); // đóng activity này lại
 //            return;
 //        }
         setContentView(R.layout.activity_quizz_question);
+//        Sự kiện Submit (Nộp bài)
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_categories_screen);
+            }
+        });
+//        Dữ liệu mẫu
+        lvQuizz = findViewById(R.id.lvQuizz);
         arrQuizzs = new ArrayList<>();
         arrQuizzs.add(new Quizz_items("Câu 1","What is the capital of VietNam?","A. LonDon","B. HaNoi","C. Paris","D. Tokyo"));
         arrQuizzs.add(new Quizz_items("Câu 2","1 + 1 = ?","A. 11","B. 2","C. 3","D. 0"));
